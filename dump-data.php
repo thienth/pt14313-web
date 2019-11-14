e5a9cc2ceddac1c633dd2b43614720079eb57237
thienth32 <thienth32@gmail.com>
2019-11-14 18:21:39 +0700

    using faker lib

--- dump-data.php
+++ dump-data.php
@@ -1,5 +1,13 @@
 <?php 
 require_once './commons/db.php';
+//https://github.com/fzaninotto/Faker
+require_once './libs/Faker/autoload.php';
+$faker = Faker\Factory::create();
+// $name = $faker->name('female');
+// $companyName = $faker->company;
+// echo $name. "-" . $companyName;
+// die;
+
 // tạo dump data cho bảng roles
 $roles = [
 	['name' => 'member', 'status' => 1],
@@ -37,6 +45,15 @@ foreach ($users as $value) {
 	$sqlQuery = "insert into users (name, email, password, role_id)
 			values ('$name', '$email', '$password', $role_id)";
 	// executeQuery($sqlQuery);
+}
+
+for ($i=0; $i < 10; $i++) { 
+	$name = $faker->name;
+	$desc = $faker->realText;
+	$showMenu = rand(0, 1);
+
+	// thực hiện insert dữ liệu cho bảng categories
+
 }
 
 
